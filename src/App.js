626e59071e7395659a9d408fa9ac66a8df68e54e
Dagmar87 <jdfssobrinho@hotmail.com>
2024-12-09 10:29:54 -0300

    Adição do marker nos codigos do App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
-import { Icon } from 'leaflet';
+import { icon, Icon } from 'leaflet';
 import * as parkData from "./data/skateboard-parks.json"
 
-function App() {
-  return (
+export default function App() {
+
+	const [activePark, setActivePark] = useState(null);
+
+	return (
 		<MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
 			<TileLayer
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			/>
-		</MapContainer>    
-  );
+			{
+				parkData.features.map(park => (
+					<Marker
+						key={park.properties.PARK_ID}
+						position={[
+							park.geometry.coordinates[1],
+							park.geometry.coordinates[0]
+						]}
+						onClick={() => {
+							setActivePark(park);
+						}}
+						icon={icon}
+					/>
+				))
+			}
+		</MapContainer>
+	);
 }
 
 export default App;
